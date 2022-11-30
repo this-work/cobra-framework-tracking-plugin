e4708615a3b810163ebffb8911d750a614c82978
cwThis <charly.wolff@this.work>
2022-11-30 14:25:03 +0100

    fix adblocker blocks website usage

--- src/plugin.js
+++ src/plugin.js
@@ -1,7 +1,7 @@
 /**
  * Cobra-Framework Vue 2 tracking plugin injections
  *
- * Version 1.0.0
+ * Version 1.0.1
  * Author Tobias Wöstmann
  *
  */
@@ -11,39 +11,59 @@ export default ({ app }, inject) => {
     const endpoints = {
 
         async pageEnter(entryId, pageUrl) {
-            const { id } = await app.$axios.$post('/api/tracking/page/enter', {
-                'eventType': 'PageView',
-                'entryId': entryId,
-                'pageUrl': pageUrl
-            });
-            sessionStorage.setItem('trackingPageId', id);
-            return id;
+            try {
+                const { id } = await app.$axios.$post('/api/tracking/page/enter', {
+                    'eventType': 'PageView',
+                    'entryId': entryId,
+                    'pageUrl': pageUrl
+                });
+                sessionStorage.setItem('trackingPageId', id);
+                return id;
+            } catch(err) {
+                console.info('tracking blocked');
+            }
         },
 
         async pageLeave( pageId = null ) {
-            return await app.$axios.$post('/api/tracking/page/leave', {
-                'pageId': pageId
-            });
+            try {
+                return await app.$axios.$post('/api/tracking/page/leave', {
+                    'pageId': pageId
+                });
+            } catch(err) {
+                console.info('tracking blocked');
+            }
         },
 
         async download(url) {
-            return await app.$axios.$post('/api/tracking/download-event', {
-                'downloadUrl': url
-            });
+            try {
+                return await app.$axios.$post('/api/tracking/download-event', {
+                    'downloadUrl': url
+                });
+            } catch(err) {
+                console.info('tracking blocked');
+            }
         },
 
         async identity() {
-            const { systemUserId } = await app.$axios.$post('/api/tracking/identity');
-            sessionStorage.setItem('trackingSystemUserId', systemUserId);
-            return systemUserId;
+            try {
+                const { systemUserId } = await app.$axios.$post('/api/tracking/identity');
+                sessionStorage.setItem('trackingSystemUserId', systemUserId);
+                return systemUserId;
+            } catch(err) {
+                console.info('tracking blocked');
+            }
         },
 
         ping() {
-            const data = new FormData();
-            if (sessionStorage.getItem('trackingPageId')) {
-                data.append('pageId', sessionStorage.getItem('trackingPageId'));
+            try {
+                const data = new FormData();
+                if (sessionStorage.getItem('trackingPageId')) {
+                    data.append('pageId', sessionStorage.getItem('trackingPageId'));
+                }
+                return navigator.sendBeacon('/api/tracking/ping', data);
+            } catch(err) {
+                console.info('tracking blocked');
             }
-            return navigator.sendBeacon('/api/tracking/ping', data);
         }
 
     };
