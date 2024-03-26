152c27ce86b2761faa3a2ec424e11eb52f59c592
Tobias Wöstmann <tobias.woestmann@this.work>
2024-03-26 15:18:03 +0100

    update tracking plugin

--- src/plugin.js
+++ src/plugin.js
@@ -6,6 +6,8 @@
  *
  */
 
+import { completeUnit } from '@this/cobra-framework-companion-plugin/src/plugins/vanilla/status-helper';
+
 export default ({ app }, inject) => {
 
     const endpoints = {
@@ -73,6 +75,67 @@ export default ({ app }, inject) => {
     document.addEventListener('visibilitychange', () => {
         if (document.visibilityState !== 'hidden') return;
         endpoints.ping();
+    });
+
+    document.addEventListener('playlist-completed', async ({ detail }) => {
+            try {
+                await app.$axios.$post('/api/tracking/chapter/completion', {
+                    'playlistId': detail.id,
+                    'siteId': app.i18n.localeProperties.siteId,
+                    'completed': true,
+                    [sessionStorage.getItem('csrfTokenName')]: sessionStorage.getItem('csrfTokenValue')
+                });
+            } catch (err) {
+                console.log(err);
+            }
+    });
+
+    document.addEventListener('playlist-completed', async ({ detail }) => {
+
+        const courseList = await app.$axios.$get('api/courselist');
+        const completedPlaylists = sessionStorage.getItem('completed').split(',');
+
+        const courseListArray = Object.entries(courseList);
+
+        const filteredCourseListArray = courseListArray.filter(([key, value]) => value.includes(parseInt(detail.id)));
+
+        if (filteredCourseListArray.length > 0) {
+
+            const completedCourseListArray = filteredCourseListArray.filter(([key, value]) => value.every(v => completedPlaylists.includes(v + '')));
+
+            if (completedCourseListArray.length > 0) {
+                for (const completedCourse of Object.keys(Object.fromEntries(completedCourseListArray))) {
+
+                    completeUnit(completedCourse);
+
+                    try {
+                        await app.$axios.$post('/api/tracking/course/completion', {
+                            'courseId': completedCourse,
+                            'siteId': app.i18n.localeProperties.siteId,
+                            'completed': true,
+                            [sessionStorage.getItem('csrfTokenName')]: sessionStorage.getItem('csrfTokenValue')
+                        });
+                    } catch (err) {
+                        console.log(err);
+                    }
+
+                }
+            }
+        }
+
+    });
+
+    document.addEventListener('quiz-attempt', async ({ detail }) => {
+        try {
+            await app.$axios.$post('/api/tracking/quiz/attempt', {
+                [sessionStorage.getItem('csrfTokenName')]: sessionStorage.getItem('csrfTokenValue'),
+                'quizId': detail.id,
+                'siteId': app.i18n.localeProperties.siteId,
+                ...detail
+            });
+        } catch (err) {
+            console.log(err);
+        }
     });
 
     endpoints.identity();
--- src/proxy.js
+++ src/proxy.js
@@ -16,11 +16,15 @@ export default function() {
      * List all extra proxy routes
      */
     const proxyRoutes = [
+        { '/api/courselist': 'api/v1/courselist' },
         { '/api/tracking/page/enter': '/actions/tracking/page-tracking/enter' },
         { '/api/tracking/page/leave': '/actions/tracking/page-tracking/leave' },
         { '/api/tracking/identity': '/actions/tracking/user-tracking' },
         { '/api/tracking/ping': '/actions/tracking/session-tracking/session-ping' },
-        { '/api/tracking/download-event': 'actions/tracking/download-tracking' }
+        { '/api/tracking/download-event': 'actions/tracking/download-tracking' },
+        { '/api/tracking/course/completion': 'api/v1/course/track-completion-state' },
+        { '/api/tracking/chapter/completion': 'api/v1/playlist/track-completion-state'},
+        { '/api/tracking/quiz/attempt': 'api/v1/quiz/track-attempt'},
     ];
 
 
