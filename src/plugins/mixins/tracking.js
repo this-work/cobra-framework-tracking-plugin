b904c071d190148252f6b85de41a69f3f168d0f4
Tobias Wöstmann <tobias.woestmann@this.work>
2024-04-23 16:55:07 +0200

    track pages only when logged in

--- src/plugins/mixins/tracking.js
+++ src/plugins/mixins/tracking.js
@@ -1,20 +1,26 @@
 export default {
     beforeRouteEnter(to, from, next) {
         next(async ctx => {
-            const { $tracking, data, id } = ctx;
-            const entryId = id ?? data?.id ?? data.find(Boolean)?.id ?? null;
-            await $tracking.pageEnter(entryId, to.fullPath);
+            const { $auth, $tracking, data, id } = ctx;
+            if ($auth.loggedIn) {
+                const entryId = id ?? data?.id ?? data.find(Boolean)?.id ?? null;
+                await $tracking.pageEnter(entryId, to.fullPath);
+            }
         });
     },
     async beforeRouteUpdate(to, from, next) {
-        this.$tracking.pageLeave(sessionStorage.getItem('trackingPageId'));
-        const entryId = this?.id ?? this.data?.id ?? this.data.find(Boolean)?.id ?? null;
-        await this.$tracking.pageEnter(entryId, to.fullPath);
+        if (this.$auth.loggedIn) {
+            this.$tracking.pageLeave(sessionStorage.getItem('trackingPageId'));
+            const entryId = this?.id ?? this.data?.id ?? this.data.find(Boolean)?.id ?? null;
+            await this.$tracking.pageEnter(entryId, to.fullPath);
+        }
 
         next();
     },
     async beforeRouteLeave(to, from, next) {
-        this.$tracking.pageLeave(sessionStorage.getItem('trackingPageId'));
+        if (this.$auth.loggedIn) {
+            this.$tracking.pageLeave(sessionStorage.getItem('trackingPageId'));
+        }
 
         next();
     }
